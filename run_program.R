1d6bcfd426fc0ca9feffb93bca24e022f1cec2d9
mingkaijiang <shanyujordan512@gmail.com>
2020-05-01 09:18:50 +1000

    setting up

---
+++ run_program.R
@@ -0,0 +1,15 @@
+#### This is the master script to process the WTC drought x CO2 dataset
+####
+#### author: Mingkai Jiang
+####         m.jiang@westernsydney.edu.au
+####
+
+############################# set up #################################
+#### clear wk space
+rm(list=ls(all=TRUE))
+
+#### read in necessary stuffs
+source("prepare.R")
+
+
+############################# set up #################################
