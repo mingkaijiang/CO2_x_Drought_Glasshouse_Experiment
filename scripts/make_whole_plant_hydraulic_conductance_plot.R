b0bdfa42639982e3034a09a00eaec73d3c815254
mingkaijiang <shanyujordan512@gmail.com>
2020-05-11 13:02:45 +1000

    log plots finished

--- scripts/make_whole_plant_hydraulic_conductance_plot.R
+++ scripts/make_whole_plant_hydraulic_conductance_plot.R
@@ -7,10 +7,20 @@ make_whole_plant_hydraulic_conductance_plot <- function() {
     
     ### plotting DFs
     plotDF1 <- summaryBy(E_psi~Trt+Day, FUN=c(mean, se), 
-                         data=pilDF, na.rm=T, keep.names=T)
+                         data=pilDF, keep.names=T)
     
     plotDF2 <- summaryBy(E_psi~Trt+Day, FUN=c(mean, se), 
-                         data=popDF, na.rm=T, keep.names=T)
+                         data=popDF, keep.names=T)
+    
+    ### calculating logs
+    plotDF1$log_E_psi <- log(plotDF1$E_psi.mean)
+    plotDF1$log_E_psi_pos <- log(plotDF1$E_psi.mean + plotDF1$E_psi.se)
+    plotDF1$log_E_psi_neg <- log(plotDF1$E_psi.mean - plotDF1$E_psi.se)
+    
+    plotDF2$log_E_psi <- log(plotDF2$E_psi.mean)
+    plotDF2$log_E_psi_pos <- log(plotDF2$E_psi.mean + plotDF2$E_psi.se)
+    plotDF2$log_E_psi_neg <- log(plotDF2$E_psi.mean - plotDF2$E_psi.se)
+    
     
     #### plotting
     p1 <- ggplot(plotDF1, aes(x=Day, y=E_psi.mean, group=Trt)) +
@@ -156,6 +166,150 @@ make_whole_plant_hydraulic_conductance_plot <- function() {
               ncol=1, rel_heights=c(1, 0.1))
     dev.off() 
     
+    
+    #### plotting
+    p1 <- ggplot(plotDF1, aes(x=Day, y=log_E_psi, group=Trt)) +
+        geom_point(aes(col=Trt, fill=Trt, pch=Trt), size=2)+
+        geom_errorbar(aes(col=Trt, x=Day, 
+                          ymin=log_E_psi_neg, ymax=log_E_psi_pos),
+                      width=0.2)+
+        geom_line(aes(col=Trt, lty=Trt))+
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.text.x=element_text(size=12),
+              axis.title.x=element_blank(),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=14),
+              legend.title=element_text(size=16),
+              panel.grid.major=element_blank(),
+              legend.position="none",
+              legend.box = 'horizontal',
+              legend.box.just = 'left',
+              plot.title = element_text(size=16, face="bold.italic", 
+                                        hjust = 0.5))+
+        ylab(expression(paste("log E / "*psi[pd] * "-" * psi[md] * 
+                                  " (l " * d^-1 * " " * MPa^-1 * ")")))+
+        scale_color_manual(name="",
+                           limits=c("PILAD", "PILAND", "PILED", "PILEND"),
+                           labels=c(expression(paste(aC[a]*" - D")), 
+                                    expression(paste(aC[a]*" - W")),
+                                    expression(paste(eC[a]*" - D")),
+                                    expression(paste(eC[a]*" - W"))),
+                           values=c("blue3", "blue3", "red2", "red2"),
+                           guide=guide_legend(nrow=1))+
+        scale_fill_manual(name="",
+                          limits=c("PILAD", "PILAND", "PILED", "PILEND"),
+                          labels=c(expression(paste(aC[a]*" - D")), 
+                                   expression(paste(aC[a]*" - W")),
+                                   expression(paste(eC[a]*" - D")),
+                                   expression(paste(eC[a]*" - W"))),
+                          values=c("white", "blue3", "white", "red2"),
+                          guide=guide_legend(nrow=1))+
+        scale_linetype_manual(name="",
+                              limits=c("PILAD", "PILAND", "PILED", "PILEND"),
+                              labels=c(expression(paste(aC[a]*" - D")), 
+                                       expression(paste(aC[a]*" - W")),
+                                       expression(paste(eC[a]*" - D")),
+                                       expression(paste(eC[a]*" - W"))),
+                              values=c("dotted", "solid", "dotted", "solid"))+
+        scale_shape_manual(name="",
+                           limits=c("PILAD", "PILAND", "PILED", "PILEND"),
+                           labels=c(expression(paste(aC[a]*" - D")), 
+                                    expression(paste(aC[a]*" - W")),
+                                    expression(paste(eC[a]*" - D")),
+                                    expression(paste(eC[a]*" - W"))),
+                           values=c(21,21,21,21))+
+        ggtitle("E. pilularis")+
+        ylim(-4, 4)+
+        xlab("Day")+
+        guides(fill = guide_legend(override.aes = list(shape = c(21, 21, 21, 21),
+                                                       fill = c("white", "blue3", "white", "red2"),
+                                                       col = c("blue3", "blue3", "red2", "red2"),
+                                                       linetype = c("dotted", "dotted", "solid", "solid"))))+
+        scale_x_continuous(limits=c(0, 10),
+                           breaks=c(0, 2, 4, 6, 8, 10))
+    
+    
+    
+    p2 <- ggplot(plotDF2, aes(x=Day, y=log_E_psi, group=Trt)) +
+        geom_point(aes(col=Trt, fill=Trt, pch=Trt), size=2)+
+        geom_errorbar(aes(col=Trt, x=Day, 
+                          ymin=log_E_psi_neg, ymax=log_E_psi_pos),
+                      width=0.2)+
+        geom_line(aes(col=Trt, lty=Trt))+
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.text.x=element_text(size=12),
+              axis.title.x=element_blank(),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_blank(),
+              legend.text=element_text(size=14),
+              legend.title=element_text(size=16),
+              panel.grid.major=element_blank(),
+              legend.position="none",
+              legend.box = 'horizontal',
+              legend.box.just = 'left',
+              plot.title = element_text(size=16, face="bold.italic", 
+                                        hjust = 0.5))+
+        ylab(expression(paste("log E / "*psi[pd] * "-" * psi[md] * 
+                                  " (l " * d^-1 * " " * MPa^-1 * ")")))+
+        scale_color_manual(name="",
+                           limits=c("POPAD", "POPAND", "POPED", "POPEND"),
+                           labels=c(expression(paste(aC[a]*" - D")), 
+                                    expression(paste(aC[a]*" - W")),
+                                    expression(paste(eC[a]*" - D")),
+                                    expression(paste(eC[a]*" - W"))),
+                           values=c("blue3", "blue3", "red2", "red2"),
+                           guide=guide_legend(nrow=1))+
+        scale_fill_manual(name="",
+                          limits=c("POPAD", "POPAND", "POPED", "POPEND"),
+                          labels=c(expression(paste(aC[a]*" - D")), 
+                                   expression(paste(aC[a]*" - W")),
+                                   expression(paste(eC[a]*" - D")),
+                                   expression(paste(eC[a]*" - W"))),
+                          values=c("white", "blue3", "white", "red2"),
+                          guide=guide_legend(nrow=1))+
+        scale_linetype_manual(name="",
+                              limits=c("POPAD", "POPAND", "POPED", "POPEND"),
+                              labels=c(expression(paste(aC[a]*" - D")), 
+                                       expression(paste(aC[a]*" - W")),
+                                       expression(paste(eC[a]*" - D")),
+                                       expression(paste(eC[a]*" - W"))),
+                              values=c("dotted", "solid", "dotted", "solid"))+
+        scale_shape_manual(name="",
+                           limits=c("POPAD", "POPAND", "POPED", "POPEND"),
+                           labels=c(expression(paste(aC[a]*" - D")), 
+                                    expression(paste(aC[a]*" - W")),
+                                    expression(paste(eC[a]*" - D")),
+                                    expression(paste(eC[a]*" - W"))),
+                           values=c(21,21,21,21))+
+        ggtitle("E. populnea")+
+        ylim(-4, 4)+
+        xlab("Day")+
+        guides(fill = guide_legend(override.aes = list(shape = c(21, 21, 21, 21),
+                                                       fill = c("white", "blue3", "white", "red2"),
+                                                       col = c("blue3", "blue3", "red2", "red2"),
+                                                       linetype = c("dotted", "dotted", "solid", "solid"))))+
+        scale_x_continuous(limits=c(0, 40),
+                           breaks=c(0, 5, 10, 20, 30, 40))
+    
+    ### output
+    combined_legend <- get_legend(p1 + theme(legend.position="bottom",
+                                             legend.box = 'vertical',
+                                             legend.box.just = 'left'))
+    
+    
+    combined_plots <- plot_grid(p1, p2, 
+                                labels=c("(a)", "(b)"), 
+                                ncol=2, align="h", axis = "l",
+                                label_x=0.16, label_y=0.9)
+    
+    
+    pdf(paste0(outdir, "F7.whole_plant_hydraulics_log_plot.pdf"), width=8, height=4)
+    plot_grid(combined_plots, combined_legend, 
+              ncol=1, rel_heights=c(1, 0.1))
+    dev.off() 
     
     
 
