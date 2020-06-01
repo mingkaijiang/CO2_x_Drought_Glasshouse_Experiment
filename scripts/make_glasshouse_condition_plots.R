f646a97604063f25cca688156a69ab75e70c27df
mingkaijiang <shanyujordan512@gmail.com>
2020-06-02 08:40:24 +1000

    updating figures

--- scripts/make_glasshouse_condition_plots.R
+++ scripts/make_glasshouse_condition_plots.R
@@ -10,9 +10,23 @@ make_glasshouse_condition_plots <- function() {
     plotDF1 <- rbind(myDF1, myDF3)
     plotDF2 <- rbind(myDF2, myDF3)
     
-    colnames(plotDF1) <- colnames(plotDF2) <- c("DateTime", "Tair", "RH", "SVP",
-                                                "RHdeficit", "VPD", "DAY",
-                                                "GH", "CO2")
+    plotDF <- rbind(plotDF1, plotDF2)
+    
+    colnames(plotDF) <- colnames(plotDF1) <- colnames(plotDF2) <- c("DateTime", "Tair", "RH", "SVP",
+                                                                    "RHdeficit", "VPD", "DAY",
+                                                                    "GH", "CO2")
+    
+    ### setting date and time
+    plotDF$DateTime <- paste0(plotDF$DateTime, ":00")
+    plotDF$DateTime <- as.character(plotDF$DateTime)
+    plotDF$Date <- sub(" .*", "", plotDF$DateTime)
+    plotDF$Date <- as.Date(as.character(plotDF$Date), format="%m/%d/%Y")
+    plotDF$Time <- sub(".+? ", "", plotDF$DateTime)
+    plotDF$Time <- chron(times=plotDF$Time)    
+    plotDF$DateTime <- as.POSIXct(paste0(as.character(plotDF$Date), " ", 
+                                          as.character(plotDF$Time)),
+                                   format="%Y-%m-%d %H:%M:%S")
+    
     
     ### setting date and time
     plotDF1$DateTime <- paste0(plotDF1$DateTime, ":00")
@@ -34,6 +48,7 @@ make_glasshouse_condition_plots <- function() {
     plotDF2$DateTime <- as.POSIXct(paste0(as.character(plotDF2$Date), " ", 
                                           as.character(plotDF2$Time)),
                                    format="%Y-%m-%d %H:%M:%S")
+
     
     ### plotting
     p1 <- ggplot(plotDF1, aes(x=DateTime, y=Tair, group=CO2)) +
@@ -250,7 +265,121 @@ make_glasshouse_condition_plots <- function() {
                                  label_x=0.08, label_y=0.85)
     
     
-    pdf(paste0(outdir, "F1.glasshouse_condition.pdf"), width=16, height=14)
+    
+    ### plotting combined data frame
+    p1 <- ggplot(plotDF, aes(x=DateTime, y=Tair, group=CO2)) +
+        geom_point(aes(color=CO2), pch=19)+
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.text.x=element_blank(),
+              axis.title.x=element_blank(),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=24),
+              legend.title=element_text(size=20),
+              panel.grid.major=element_blank(),
+              legend.position="none",
+              legend.box = 'horizontal',
+              legend.box.just = 'left',
+              plot.title = element_text(size=30, face="bold", 
+                                        hjust = 0.5))+
+        ylab(expression(paste("Temperature ("*degree*"C)")))+
+        scale_shape_manual(name=expression(CO[2]),
+                           limits=c("A", "E"),
+                           labels=c("amb", "ele"),
+                           values=c(19,19),
+                           guide=guide_legend(nrow=1))+
+        scale_color_manual(name=expression(CO[2]),
+                           limits=c("A", "E"),
+                           labels=c("amb", "ele"),
+                           values=c("blue3", "red2"),
+                           guide=guide_legend(nrow=1))+
+        scale_x_datetime(name="Date", 
+                         breaks=date_breaks("24 hour"),
+                         labels=date_format("%m-%d"))+
+        ylim(10, 35)+
+        guides(fill = guide_legend(override.aes = list(shape = c(19, 19),
+                                                       fill = c("blue3", "red2"),
+                                                       size= c(10, 10))))
+    
+    
+    p3 <- ggplot(plotDF1, aes(x=DateTime, y=RH, group=CO2)) +
+        geom_point(aes(color=CO2), pch=19)+
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.text.x=element_blank(),
+              axis.title.x=element_blank(),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="none",
+              legend.box = 'horizontal',
+              legend.box.just = 'left',
+              plot.title = element_text(size=14, face="bold", 
+                                        hjust = 0.5))+
+        ylab("Relative Humidity (%)")+
+        scale_shape_manual(name=expression(CO[2]),
+                           limits=c("A", "E"),
+                           labels=c("amb", "ele"),
+                           values=c(19,19),
+                           guide=guide_legend(nrow=1))+
+        scale_color_manual(name=expression(CO[2]),
+                           limits=c("A", "E"),
+                           labels=c("amb", "ele"),
+                           values=c("blue3", "red2"),
+                           guide=guide_legend(nrow=1))+
+        scale_x_datetime(name="Date", 
+                         breaks=date_breaks("24 hour"),
+                         labels=date_format("%m-%d"))+
+        ylim(35, 100)
+    
+    p5 <- ggplot(plotDF1, aes(x=DateTime, y=VPD, group=CO2)) +
+        geom_point(aes(color=CO2), pch=19)+
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.text.x=element_text(size=12),
+              axis.title.x=element_text(size=14),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              panel.grid.major=element_blank(),
+              legend.position="none",
+              legend.box = 'horizontal',
+              legend.box.just = 'left',
+              plot.title = element_text(size=14, face="bold", 
+                                        hjust = 0.5))+
+        ylab("Vapor Pressure Deficit (kPa)")+
+        scale_shape_manual(name=expression(CO[2]),
+                           limits=c("A", "E"),
+                           labels=c("amb", "ele"),
+                           values=c(19,19),
+                           guide=guide_legend(nrow=1))+
+        scale_color_manual(name=expression(CO[2]),
+                           limits=c("A", "E"),
+                           labels=c("amb", "ele"),
+                           values=c("blue3", "red2"),
+                           guide=guide_legend(nrow=1))+
+        scale_x_datetime(name="Date", 
+                         breaks=date_breaks("24 hour"),
+                         labels=date_format("%m-%d"))+
+        ylim(0,3)
+    
+    
+    ### output
+    combined_legend <- get_legend(p1 + theme(legend.position="bottom",
+                                             legend.box = 'vertical',
+                                             legend.box.just = 'left'))
+    
+    
+    combined_plots <- plot_grid(p1, p3, p5,
+                                labels=c("(a)", "(b)", "(c)"), 
+                                ncol=1, align="vh", axis = "l",
+                                label_x=0.08, label_y=0.85)
+    
+    pdf(paste0(outdir, "F1.glasshouse_condition.pdf"), width=8, height=12)
     plot_grid(combined_plots, combined_legend, 
               ncol=1, rel_heights=c(1, 0.1))
     dev.off() 
--- scripts/make_transpiration_leaf_area_plot.R
+++ scripts/make_transpiration_leaf_area_plot.R
@@ -1,27 +1,34 @@
 make_transpiration_leaf_area_plot <- function() {
    
    ### read in data
-   PilTLA<-read.csv("data/glasshouse2/PILtransp_leafarea.csv",sep=",", header=TRUE)
-   PopTLA<-read.csv("data/glasshouse2/POPtransp_leafarea.csv",sep=",", header=TRUE)
+   pilDF<-read.csv("data/glasshouse2/PILtransp_leafarea.csv",sep=",", header=TRUE)
+   popDF<-read.csv("data/glasshouse2/POPtransp_leafarea.csv",sep=",", header=TRUE)
    
+   
+   #### obtain summary DF
+   sumDF1 <- summaryBy(leaf_area+transp_plant~Trt, FUN=c(mean,se),
+                       data=pilDF, keep.names=T)
+   
+   sumDF2 <- summaryBy(leaf_area+transp_plant~Trt, FUN=c(mean,se),
+                       data=popDF, keep.names=T)
    
    ############################# perform statistical tests ##############################
    ### perform linear mixed effect model statistics 
-   mod1 <- lmer(leaf_area ~ CO2 * H2O + (1|Glasshouse), data=PilTLA)
+   mod1 <- lmer(leaf_area ~ CO2 * H2O + (1|Glasshouse), data=pilDF)
    outDF1 <- anova(mod1)
    write.csv(outDF1, paste0(outdir, "statistics_leaf_area_pilularis.csv"))
    
-   mod2 <- lmer(transp_plant ~ CO2 * H2O + (1|Glasshouse), data=PilTLA)
+   mod2 <- lmer(transp_plant ~ CO2 * H2O + (1|Glasshouse), data=pilDF)
    outDF2 <- anova(mod2)
    write.csv(outDF2, paste0(outdir, "statistics_transpiration_pilularis.csv"))
    
    
    ### perform linear mixed effect model statistics on LA
-   mod1 <- lmer(leaf_area ~ CO2 * H2O + (1|Glasshouse), data=PopTLA)
+   mod1 <- lmer(leaf_area ~ CO2 * H2O + (1|Glasshouse), data=popDF)
    outDF1 <- anova(mod1)
    write.csv(outDF1, paste0(outdir, "statistics_leaf_area_populnea.csv"))
    
-   mod2 <- lmer(transp_plant ~ CO2 * H2O + (1|Glasshouse), data=PopTLA)
+   mod2 <- lmer(transp_plant ~ CO2 * H2O + (1|Glasshouse), data=popDF)
    outDF2 <- anova(mod2)
    write.csv(outDF2, paste0(outdir, "statistics_transpiration_populnea.csv"))
    
@@ -31,58 +38,157 @@ make_transpiration_leaf_area_plot <- function() {
    
    
    ################################### Plotting ######################################
-   #FIGURE 5 - Eucalyptus pilularis & populnea (Transpiration as a function of leaf area)
-   pdf(paste0(outdir, "F4.transpiration_leaf_area.pdf"), width=10, height=7.5)
-   #A
-   #bottom,left,top,right
-   par(mfrow=c(1,2), omi=c(.65, 0.75, 0.75, .65)) 
-   par(mar=c(2, 1, 4, 1))
-   par(xaxs="i",yaxs="i")
-   #PIL Transpiration vs. leaf area on day 1 of dry down
-   par(las=1)
-   with(PilTLA,plot(transp_plant[Trt=="PILAD"]~leaf_area[Trt=="PILAD"],col="blue",pch=1,cex=1.25,cex.axis=1.25,xaxt="n",ylab="",
-                    ylim=range(0,5),xlim=range(0,12)))
-   with(PilTLA,points(transp_plant[Trt=="PILAND"]~leaf_area[Trt=="PILAND"],col="blue",pch=19,cex=1.25))
+   p1 <- ggplot() +
+      geom_point(pilDF, mapping=aes(x=leaf_area, y=transp_plant, group=Trt,
+                                    col=Trt, fill=Trt, pch=Trt), size=2)+
+      geom_errorbar(sumDF1, mapping=aes(col=Trt, x=leaf_area.mean, 
+                        ymin=transp_plant.mean-transp_plant.se, 
+                        ymax=transp_plant.mean+transp_plant.se),
+                    width=0.2)+
+      geom_errorbarh(sumDF1, mapping=aes(col=Trt, xmin=leaf_area.mean-leaf_area.se, 
+                                         xmax=leaf_area.mean+leaf_area.se, 
+                                        y=transp_plant.mean),
+                    height=0.2)+
+      theme_linedraw() +
+      theme(panel.grid.minor=element_blank(),
+            axis.text.x=element_text(size=12),
+            axis.title.x=element_text(size=14),
+            axis.text.y=element_text(size=12),
+            axis.title.y=element_text(size=14),
+            legend.text=element_text(size=14),
+            legend.title=element_text(size=16),
+            panel.grid.major=element_blank(),
+            legend.position="none",
+            legend.box = 'horizontal',
+            legend.box.just = 'left',
+            plot.title = element_text(size=16, face="bold.italic", 
+                                      hjust = 0.5))+
+      ylab(expression(paste("Transpiration (l " * d^-1 * ")")))+
+      xlab(expression(paste("Leaf area (" * m^2 * ")")))+
+      scale_color_manual(name="",
+                         limits=c("PILAD", "PILAND", "PILED", "PILEND"),
+                         labels=c(expression(paste(aC[a]*" - D")), 
+                                  expression(paste(aC[a]*" - W")),
+                                  expression(paste(eC[a]*" - D")),
+                                  expression(paste(eC[a]*" - W"))),
+                         values=c("blue3", "blue3", "red2", "red2"),
+                         guide=guide_legend(nrow=1))+
+      scale_fill_manual(name="",
+                        limits=c("PILAD", "PILAND", "PILED", "PILEND"),
+                        labels=c(expression(paste(aC[a]*" - D")), 
+                                 expression(paste(aC[a]*" - W")),
+                                 expression(paste(eC[a]*" - D")),
+                                 expression(paste(eC[a]*" - W"))),
+                        values=c("white", "blue3", "white", "red2"),
+                        guide=guide_legend(nrow=1))+
+      scale_linetype_manual(name="",
+                            limits=c("PILAD", "PILAND", "PILED", "PILEND"),
+                            labels=c(expression(paste(aC[a]*" - D")), 
+                                     expression(paste(aC[a]*" - W")),
+                                     expression(paste(eC[a]*" - D")),
+                                     expression(paste(eC[a]*" - W"))),
+                            values=c("dotted", "solid", "dotted", "solid"))+
+      scale_shape_manual(name="",
+                         limits=c("PILAD", "PILAND", "PILED", "PILEND"),
+                         labels=c(expression(paste(aC[a]*" - D")), 
+                                  expression(paste(aC[a]*" - W")),
+                                  expression(paste(eC[a]*" - D")),
+                                  expression(paste(eC[a]*" - W"))),
+                         values=c(21,21,21,21))+
+      ggtitle("E. pilularis")+
+      ylim(0, 5)+
+      guides(fill = guide_legend(override.aes = list(shape = c(21, 21, 21, 21),
+                                                     fill = c("white", "blue3", "white", "red2"),
+                                                     col = c("blue3", "blue3", "red2", "red2"),
+                                                     linetype = c("dotted", "dotted", "solid", "solid"))))+
+      scale_x_continuous(limits=c(0, 10),
+                         breaks=c(0, 2, 4, 6, 8, 10))
    
-   with(PilTLA,points(transp_plant[Trt=="PILED"]~leaf_area[Trt=="PILED"],col="red",pch=1,lty=2,cex=1.25))
    
-   with(PilTLA,points(transp_plant[Trt=="PILEND"]~leaf_area[Trt=="PILEND"],col="red",pch=19,cex=1.25))
+   p2 <- ggplot() +
+      geom_point(popDF, mapping=aes(x=leaf_area, y=transp_plant, group=Trt,
+                                    col=Trt, fill=Trt, pch=Trt), size=2)+
+      geom_errorbar(sumDF2, mapping=aes(col=Trt, x=leaf_area.mean, 
+                                        ymin=transp_plant.mean-transp_plant.se, 
+                                        ymax=transp_plant.mean+transp_plant.se),
+                    width=0.04)+
+      geom_errorbarh(sumDF2, mapping=aes(col=Trt, xmin=leaf_area.mean-leaf_area.se, 
+                                         xmax=leaf_area.mean+leaf_area.se, 
+                                         y=transp_plant.mean),
+                     height=0.2)+
+      theme_linedraw() +
+      theme(panel.grid.minor=element_blank(),
+            axis.text.x=element_text(size=12),
+            axis.title.x=element_text(size=14),
+            axis.text.y=element_text(size=12),
+            axis.title.y=element_text(size=14),
+            legend.text=element_text(size=14),
+            legend.title=element_text(size=16),
+            panel.grid.major=element_blank(),
+            legend.position="none",
+            legend.box = 'horizontal',
+            legend.box.just = 'left',
+            plot.title = element_text(size=16, face="bold.italic", 
+                                      hjust = 0.5))+
+      ylab(expression(paste("Transpiration (l " * d^-1 * ")")))+
+      xlab(expression(paste("Leaf area (" * m^2 * ")")))+
+      scale_color_manual(name="",
+                         limits=c("POPAD", "POPAND", "POPED", "POPEND"),
+                         labels=c(expression(paste(aC[a]*" - D")), 
+                                  expression(paste(aC[a]*" - W")),
+                                  expression(paste(eC[a]*" - D")),
+                                  expression(paste(eC[a]*" - W"))),
+                         values=c("blue3", "blue3", "red2", "red2"),
+                         guide=guide_legend(nrow=1))+
+      scale_fill_manual(name="",
+                        limits=c("POPAD", "POPAND", "POPED", "POPEND"),
+                        labels=c(expression(paste(aC[a]*" - D")), 
+                                 expression(paste(aC[a]*" - W")),
+                                 expression(paste(eC[a]*" - D")),
+                                 expression(paste(eC[a]*" - W"))),
+                        values=c("white", "blue3", "white", "red2"),
+                        guide=guide_legend(nrow=1))+
+      scale_linetype_manual(name="",
+                            limits=c("POPAD", "POPAND", "POPED", "POPEND"),
+                            labels=c(expression(paste(aC[a]*" - D")), 
+                                     expression(paste(aC[a]*" - W")),
+                                     expression(paste(eC[a]*" - D")),
+                                     expression(paste(eC[a]*" - W"))),
+                            values=c("dotted", "solid", "dotted", "solid"))+
+      scale_shape_manual(name="",
+                         limits=c("POPAD", "POPAND", "POPED", "POPEND"),
+                         labels=c(expression(paste(aC[a]*" - D")), 
+                                  expression(paste(aC[a]*" - W")),
+                                  expression(paste(eC[a]*" - D")),
+                                  expression(paste(eC[a]*" - W"))),
+                         values=c(21,21,21,21))+
+      ggtitle("E. populnea")+
+      ylim(0, 5)+
+      guides(fill = guide_legend(override.aes = list(shape = c(21, 21, 21, 21),
+                                                     fill = c("white", "blue3", "white", "red2"),
+                                                     col = c("blue3", "blue3", "red2", "red2"),
+                                                     linetype = c("dotted", "dotted", "solid", "solid"))))+
+      scale_x_continuous(limits=c(0, 2),
+                         breaks=c(0, 0.5, 1, 1.5, 2.0))
    
-   par(las=3)
-   mtext(side = 2, text =expression(bold(Transpiration~~(l~d^-1))),
-         font=2,cex=1.0, line = 3)	
-   par(las=1)
-   mtext(side=1,text=expression(bold(Leaf~area~(m^2))),line=3,cex=1.25)
-   axis(1,labels=TRUE,tck=-0.03,cex.axis=1.25)
-   title(main="Eucalyptus pilularis",  font.main=4,cex.main=1.1,line=0.5)
-   legend("topleft",  expression(aC[a]~-~W,aC[a]~-~D,eC[a]~-~W,eC[a]~-~D),
-          cex=1.025,bty="n",
-          pch=c(19,1,19,1), col=c("blue","blue","red","red"))
+   combined_legend <- get_legend(p1 + theme(legend.position="bottom",
+                                            legend.box = 'vertical',
+                                            legend.box.just = 'left'))
    
-   #POP Transpiration vs. leaf area on day 1 of dry down
-   par(mar=c(2, 1, 4, 1))
-   par(las=1)
-   par(xaxs="i",yaxs="i")
-   with(PopTLA,plot(transp_plant[Trt=="POPAD"]~leaf_area[Trt=="POPAD"],col="blue",pch=1,cex=1.25,cex.axis=1.25,xaxt="n",ylab="",yaxt="n",
-                    ylim=range(0,5),xlim=range(0,2)))
    
-   with(PopTLA,points(transp_plant[Trt=="POPAND"]~leaf_area[Trt=="POPAND"],col="blue",pch=19,cex=1.25))
+   combined_plots <- plot_grid(p1, p2, 
+                               labels=c("(a)", "(b)"), 
+                               ncol=2, align="vh", axis = "l",
+                               label_x=0.16, label_y=0.9)
    
-   with(PopTLA,points(transp_plant[Trt=="POPED"]~leaf_area[Trt=="POPED"],col="red",pch=1,lty=2,cex=1.25))
    
-   with(PopTLA,points(transp_plant[Trt=="POPEND"]~leaf_area[Trt=="POPEND"],col="red",pch=19,cex=1.25))
+   pdf(paste0(outdir, "F4.transpiration_leaf_area.pdf"), width=8, height=4)
+   plot_grid(combined_plots, combined_legend, 
+             ncol=1, rel_heights=c(1, 0.1))
+   dev.off() 
    
-   par(las=3)
-   mtext(side = 4, text =expression(bold(Transpiration~~(l~d^-1))),
-         font=2,cex=1.0, line = 3)	
-   par(las=1)
-   axis(4,labels=TRUE,tck=-0.03,cex.axis=1.25)
-   par(las=1)
-   mtext(side=1,text=expression(bold(Leaf~area~(m^2))),line=3,cex=1.25)
-   axis(1,labels=TRUE,tck=-0.03,cex.axis=1.25)
-   title(main="Eucalyptus populnea",  font.main=4,cex.main=1.1,line=0.5)
    
-   dev.off()
    
+  
 }
 
