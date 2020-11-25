c97f0790a35c090fdf4b0c9f3979fd1f6450ce8e
mingkaijiang <shanyujordan512@gmail.com>
2020-11-25 16:23:05 +1100

    no VPD effect found

--- scripts/check_effect_of_VPD.R
+++ scripts/check_effect_of_VPD.R
@@ -1,6 +1,5 @@
 check_effect_of_VPD <- function() {
     
-    
     ### read in met data
     myDF1 <- read.csv("data/glasshouse2/met/AMBIENT_GH_TEMP_RH_PILULARIS_DRYDOWN_DAY.csv")
     myDF2 <- read.csv("data/glasshouse2/met/ELEVATED_GH_TEMP_RH_PILULARIS_DRYDOWN_DAY.csv")
@@ -23,7 +22,52 @@ check_effect_of_VPD <- function() {
                                          as.character(metDF$Time)),
                                   format="%Y-%m-%d %H:%M:%S")
     
+    ### check if there is a DAY, GH and CO2 effect on VPD
+    mod1<-lme(VPD~CO2+DAY,random=~1|GH,data=metDF)
+    summary(mod1)
+    
+    
+    ### make daily summary
+    sumDF <- summaryBy(VPD~DAY+GH+CO2, FUN=c(max, mean, se),
+                       keep.names=T, data=metDF)
+    
+    
+    ### check if there is a day, gh and CO2 effect on VPD
+    mod1<-lme(VPD.max~CO2+DAY,random=~1|GH,data=sumDF)
+    summary(mod1)
+    
+    ################################################################
+    ### result indicate VPD are similar in different CO2 and chamber
+    ################################################################
+    
+    colnames(sumDF) <- c("Day", "Glasshouse", "CO2", "VPD.max",
+                         "VPD.mean", "VPD.se")
+    
+    
+    ### read in physiologycal measurements
+    pilDF <- read.csv("data/glasshouse2/Drydown_gasexchange_pilularis_processed.csv")
+    popDF <- read.csv("data/glasshouse2/Drydown_gasexchange_populnea_processed.csv")
     
    
+    ### merge
+    pilDF2 <- merge(pilDF, sumDF, by=c("Day", "Glasshouse", "CO2"))
+    popDF2 <- merge(popDF, sumDF, by=c("Day", "Glasshouse", "CO2"))
     
+    
+    ### check stats on effect of VPD
+    
+    ### transpiration - pilularis
+    mod1<-lme(log(transp_plant)~CO2*Day*VPD.max,random=~1|Glasshouse/Replicate,
+              data=pilDF2, na.action = na.omit)
+    summary(mod1)
+    
+    ### transpiration - populnea
+    mod1<-lme(log(transp_plant)~CO2*Day*VPD.max,random=~1|Glasshouse/Replicate,
+              data=popDF2, na.action = na.omit)
+    summary(mod1)
+    
+    
+    ### no effect of VPD
+    
+    print("no VPD effect found")
 }
